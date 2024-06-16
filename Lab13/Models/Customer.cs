acd115ce2a073b9481786761e8740d97578f2f48
Myriam Roxana <91862360+MamaniLucas@users.noreply.github.com>
2024-06-15 22:22:22 -0500

    Update Customer.cs
    bool changes

--- Lab13/Models/Customer.cs
+++ Lab13/Models/Customer.cs
@@ -8,10 +8,7 @@
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string DocumentNumber { get; set; }
-
-
-        public bool activo { get; set; }
-
+        public bool activo { get; set; } = true;
 
 
     }
